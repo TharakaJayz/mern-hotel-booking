696ac9c7a8201e1e7358fb8415566c1d6bca4efe
TharakaJayz <tharakaprabhath300@gmail.com>
2025-01-01 12:01:26 +0530

    fixed navigation bug

--- client/src/pages/EditHotel.tsx
+++ client/src/pages/EditHotel.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import { ManageHotelForm } from "../forms/manageHotelForm/ManageHotelForm";
 import { useAppDispatch } from "../hooks/hooks";
@@ -8,7 +8,7 @@ import { toastActions } from "../store/Toast-slice";
 const EditHotel = () => {
   const { hotelId } = useParams();
   const dispatch = useAppDispatch();
-
+  const navigation = useNavigate();
   const { data: hotel } = useQuery("fetchMyHotelById", () =>
     apiClient.fetchMyHotelById(hotelId!),{
         enabled:!!hotelId   // query will run onlly if enable filed has true value 
@@ -18,6 +18,8 @@ const EditHotel = () => {
   const {mutate,isLoading} = useMutation(apiClient.updateMyHotelById,{
     onSuccess:()=>{
         dispatch(toastActions.add({ message: "Hotel Saved !", type: "SUCCESS" }));
+       navigation("/my-hotels");
+
     },
     onError:()=>{
         dispatch(toastActions.add({ message: "Error Saving Hotel !", type: "ERROR" }));
